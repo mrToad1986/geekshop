4238547b29b1ec1497f6914f771c00ce4583854c
mrToad <korostylev.ov@gmail.com>
2021-10-18 18:05:23 +0300

    работа с медиа

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+
 from products.views import index, products
 
 urlpatterns = [
@@ -22,3 +25,6 @@ urlpatterns = [
     path('products/', products, name = 'products'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
