434d65520561c90dd2c3918da17bd0340cff9c7b
mrToad <korostylev.ov@gmail.com>
2021-10-21 10:17:24 +0300

    загрузка товаров и категорий из БД

--- products/views.py
+++ products/views.py
@@ -1,11 +1,11 @@
-#Добавлено - загрузка динамического контента из json-файла
-import os
-import json
+#Добавлено-1 - загрузка динамического контента из json-файла
+#Добавлено-2 - загрузка изи БД вместо json-файла
 
-from django.shortcuts import render
 from datetime import datetime
+from django.shortcuts import render
+from products.models import Product, ProductCategory
 
-MODULE_DIR = os.path.dirname(__file__)
+# MODULE_DIR = os.path.dirname(__file__)
 
 # Create your views here.
 
@@ -16,11 +16,18 @@ def index(request):
 	}
 	return render(request, 'products/index.html', context)
 
-def products(request):
-	file_path = os.path.join(MODULE_DIR, 'fixtures/goods.json')
-	context = {
-	'title': 'Geekshop - Каталог',
-	'products': json.load(open(file_path, encoding='utf-8'))
-	}
+# def products(request):
+# 	file_path = os.path.join(MODULE_DIR, 'fixtures/goods.json')
+# 	context = {
+# 	'title': 'Geekshop - Каталог',
+# 	'products': json.load(open(file_path, encoding='utf-8'))
+# 	}
+# 	return render(request, 'products/products.html', context)
 
+def products(request):
+	context = {
+		'title': 'Geekshop - Каталог',
+		'products': Product.objects.all(),
+		'categories': ProductCategory.objects.all(),
+	}
 	return render(request, 'products/products.html', context)
