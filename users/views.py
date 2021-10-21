7472181a34dd4462b78e26208f9e7da1e7f90494
mrToad <korostylev.ov@gmail.com>
2021-10-21 23:21:39 +0300

    регистрация

--- users/views.py
+++ users/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import auth
+
+from users.forms import UserLoginForm
 
 def login(request):
-    context = {'title': 'Geekshop - Авторизация'}
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+
+    else:
+        form = UserLoginForm()
+
+    context = {
+        'title': 'Geekshop - Авторизация',
+        'form': form,
+    }
     return render(request, 'users/login.html', context)
 
 def registration(request):
